1a281643e00556b340be1ab849a132031e35ae17
Othmane Benhammou <othmaneb@cloud9infosystems.com>
2021-08-29 19:36:44 -0500

    add entities

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+
+        public string[] tags { get; set; }
+        public string link { get; set; }
+        public string title { get; set; }
+
+    }
+}
