0b41fb6bca9f0fadf848fe76d1a52d49b1cc71a1
Othmane Benhammou <othmaneb@cloud9infosystems.com>
2021-08-29 19:41:19 -0500

    Add Entities and HttpRequest

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using DemoAcculynx.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,7 +18,29 @@ namespace DemoAcculynx.Controllers
         {
             return View();
         }
+        public async Task<ActionResult> Items()
+        {
+            Root reservationList = new Root();
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using (var httpClient = new HttpClient(handler))
+            {
 
+                var apiUrl = ("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&tagged=C%23&site=stackoverflow&filter=!m6Wy7AN10y(E0TV5Yo_nHoqJ20Cfn6aVpiS(pp-2rJ4LZaSp.x0D45Te");
+
+                //setup HttpClient
+                httpClient.BaseAddress = new Uri(apiUrl);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //make request
+                var response = await httpClient.GetStringAsync(apiUrl);
+                reservationList = JsonConvert.DeserializeObject<Root>(response);
+
+            }
+
+
+            return View(reservationList.items);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- Models/Item.cs
+++ Models/Item.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using Newtonsoft.Json;
 
-namespace InAndOut.Models
+namespace DemoAcculynx.Models
 {
     public class Item
     {
